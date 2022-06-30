47b350df2f29475381b2f11d452873ddec975cb1
Dhruvi <dhruvijoshi45@gmail.com>
2022-06-30 22:31:43 +0530

    no message

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
-import Banner from './components/autoScrollBanner/Banner';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+// import "./components/Style.scss"
 function App() {
   return (
     <>
-    <Header/>
-    <Banner/>      
-    <Footer/>
+      <Header />
+      <div class="container">
+        <div
+          class="container__image">
+          <div class="container__info container__author">Photo by
+            <a class="link" href="https://instagram.com/silvia.diaconescu" target="_blank">
+              Silvia Diaconescu
+            </a>
+          </div>
+          <div class="container__info container__location">Geneva Lake Switzerland </div>
+        </div>
+      </div>
+      <Footer />
     </>
   );
 }
